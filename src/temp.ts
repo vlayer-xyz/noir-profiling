61af41edc02c635a4b2dbc27c8de1da81bcb6d50
Leonid Logvinov <logvinov.leon@gmail.com>
2024-01-26 14:41:26 +0100

    Remove timing for fast functions

--- src/temp.ts
+++ src/temp.ts
@@ -1,20 +1,14 @@
-import { initCircuit, readInputMap, readProof, readWitnessMap } from "./utils.js";
+import { initCircuit, readInputMap } from "./utils.js";
 import { InputMap } from "@noir-lang/noir_js";
 import { Field } from "@noir-lang/noirc_abi";
 import assert from "assert";
 
 const packageName = "poseidon";
-console.time("initCircuit for poseidon");
 const poseidon = await initCircuit(packageName);
-console.timeEnd("initCircuit for poseidon");
 
-console.time("readInputMap for poseidon");
 const poseidonInputs = await readInputMap(packageName);
-console.timeEnd("readInputMap for poseidon");
 
-console.time("poseidon.noir.execute");
 let { witness: poseidonWitness } = await poseidon.noir.execute(poseidonInputs);
-console.timeEnd("poseidon.noir.execute");
 
 console.time("poseidon.bb.generateIntermediateProof");
 const poseidonProof = await poseidon.bb.generateIntermediateProof(poseidonWitness);
@@ -22,9 +16,8 @@ console.timeEnd("poseidon.bb.generateIntermediateProof");
 
 console.time("poseidon.bb.verifyIntermediateProof");
 const poseidonProofVerification = await poseidon.bb.verifyIntermediateProof(poseidonProof);
-console.timeEnd("poseidon.bb.verifyIntermediateProof");
-
 assert(poseidonProofVerification, "Poseidon proof verification failed");
+console.timeEnd("poseidon.bb.verifyIntermediateProof");
 
 console.time("generateIntermediateProofArtifacts");
 const numPublicInputs = 1;
@@ -34,16 +27,10 @@ const { proofAsFields, vkAsFields, vkHash } = await poseidon.bb.generateIntermed
 );
 console.timeEnd("generateIntermediateProofArtifacts");
 
-console.time("poseidon.noir.destroy");
 await poseidon.noir.destroy();
-console.timeEnd("poseidon.noir.destroy");
 
 // RECURSIVE PROOF
-console.time("initCircuit for recursive_poseidon");
 const recursivePoseidon = await initCircuit("recursive_poseidon");
-console.timeEnd("initCircuit for recursive_poseidon");
-
-console.time("recursivePoseidon.noir.execute");
 const recursionInputs: InputMap = {
   verification_key: vkAsFields,
   proof: proofAsFields,
@@ -51,7 +38,6 @@ const recursionInputs: InputMap = {
   key_hash: vkHash,
 };
 const { witness: recursivePoseidonWitness } = await recursivePoseidon.noir.execute(recursionInputs);
-console.timeEnd("recursivePoseidon.noir.execute");
 
 console.time("recursivePoseidon.bb.generateFinalProof");
 const recursivePoseidonProof = await recursivePoseidon.bb.generateFinalProof(recursivePoseidonWitness);
@@ -59,9 +45,7 @@ console.timeEnd("recursivePoseidon.bb.generateFinalProof");
 
 console.time("recursivePoseidon.bb.verifyFinalProof");
 const recursivePoseidonProofVerification = await recursivePoseidon.bb.verifyFinalProof(recursivePoseidonProof);
-console.timeEnd("recursivePoseidon.bb.verifyFinalProof");
 assert(recursivePoseidonProofVerification, "Recursive Poseidon proof verification failed");
+console.timeEnd("recursivePoseidon.bb.verifyFinalProof");
 
-console.time("recursivePoseidon.noir.destroy");
 await recursivePoseidon.noir.destroy();
-console.timeEnd("recursivePoseidon.noir.destroy");
