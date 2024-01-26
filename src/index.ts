f887d190ed845681bb1f39e3aafcdeb19b033e4f
Leonid Logvinov <logvinov.leon@gmail.com>
2024-01-26 14:12:42 +0100

    Slight refactor

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,12 @@
-import {
-  readInputMap,
-  readProof,
-  readWitnessMap,
-  readCircuit,
-} from "./utils.js";
+import { readInputMap, readProof, readWitnessMap, initCircuit } from "./utils.js";
 import assert from "assert";
-import os from "os";
-import { BarretenbergBackend } from "@noir-lang/backend_barretenberg";
-import { Noir } from "@noir-lang/noir_js";
 import { Action, argv } from "./cli.js";
 
-const PROOF_PATH = `./proofs/${argv.package}.proof`;
-const VERIFIER_MAP_PATH = `./circuits/${argv.package}/Verifier.toml`;
-const PROVER_MAP_PATH = `./circuits/${argv.package}/Prover.toml`;
-
-const circuit = await readCircuit(argv.package);
-const backend = new BarretenbergBackend(circuit, {
-  threads: os.cpus().length,
-});
-const noir = new Noir(circuit, backend);
+const { noir, circuit } = await initCircuit(argv.package);
 
 if (argv.action == Action.Verify) {
-  let proof = await readProof(PROOF_PATH);
-  let witnessMap = await readWitnessMap(VERIFIER_MAP_PATH, circuit.abi);
+  let proof = await readProof(argv.package);
+  let witnessMap = await readWitnessMap(argv.package, circuit.abi);
   const proofData = {
     proof,
     publicInputs: Array.from(witnessMap.values()),
@@ -30,7 +14,7 @@ if (argv.action == Action.Verify) {
   let isCorrect = await noir.verifyFinalProof(proofData);
   assert(isCorrect, "Proof vefification failed");
 } else if (argv.action == Action.Prove) {
-  let inputMap = await readInputMap(PROVER_MAP_PATH);
+  let inputMap = await readInputMap(argv.package);
   await noir.generateFinalProof(inputMap);
 }
 
