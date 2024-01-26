ad0a3ffea6c6d652f52a77af5e716650606017dc
Leonid Logvinov <logvinov.leon@gmail.com>
2024-01-26 15:46:51 +0100

    Add recursive_tree script

---
+++ src/recursive_tree.ts
@@ -0,0 +1,43 @@
+import { initCircuit, readInputMap } from "./utils.js";
+import { InputMap } from "@noir-lang/noir_js";
+import { Field } from "@noir-lang/noirc_abi";
+import assert from "assert";
+import { prepareIntermediateProofArtefacts } from "./recursive_utils.js";
+
+const packageName = "keccak";
+const lhsProof = await prepareIntermediateProofArtefacts(packageName);
+const rhsProof = await prepareIntermediateProofArtefacts(packageName);
+
+// RECURSIVE PROOF
+const recursive2x = await initCircuit("recursive_2x");
+const lhsPublicInputs = [(await readInputMap(packageName)).x as Field];
+const rhsPublicInputs = [(await readInputMap(packageName)).x as Field];
+const lhsRecursionInputs: InputMap = {
+  verification_key: lhsProof.vkAsFields,
+  proof: lhsProof.proofAsFields,
+  public_inputs: lhsPublicInputs,
+  key_hash: lhsProof.vkHash,
+};
+const rhsRecursionInputs: InputMap = {
+  verification_key: rhsProof.vkAsFields,
+  proof: rhsProof.proofAsFields,
+  public_inputs: rhsPublicInputs,
+  key_hash: rhsProof.vkHash,
+};
+
+const publicInputs = {
+  lhs_proof: lhsRecursionInputs,
+  rhs_proof: rhsRecursionInputs,
+};
+const { witness } = await recursive2x.noir.execute(publicInputs);
+
+console.time("recursive2x.bb.generateFinalProof");
+const proof = await recursive2x.bb.generateFinalProof(witness);
+console.timeEnd("recursive2x.bb.generateFinalProof");
+
+console.time("recursive2x.bb.verifyFinalProof");
+const proofVerification = await recursive2x.bb.verifyFinalProof(proof);
+assert(proofVerification, "Recursive2x Poseidon proof verification failed");
+console.timeEnd("recursive2x.bb.verifyFinalProof");
+
+await recursive2x.noir.destroy();
