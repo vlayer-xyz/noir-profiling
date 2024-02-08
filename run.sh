9910ff89ac2283967b6607fb1dfcfe6bf65b4454
Leonid Logvinov <logvinov.leon@gmail.com>
2024-02-08 15:50:04 +0100

    Add noir_tests

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # Define a list of packages separated by spaces
-packages="add_mul loop if"
+packages="if"
 
 # Loop over each package
 for prog in $packages; do
@@ -7,7 +7,11 @@ for prog in $packages; do
     cat circuits/$prog/src/main.nr
     nargo compile --package $prog
     nargo info --package $prog
-    jq -r ".bytecode" target/$prog.json | base64 -d > target/$prog.acir
+    jq -r ".bytecode" target/$prog.json | base64 -d > target/$prog.acir.gz
+    gunzip < target/$prog.acir.gz > target/$prog.acir
+    hexdump -C target/$prog.acir
+    # jq -r ".bytecode" target/$prog.json > target/$prog.acir
+    # ../barretenberg/cpp/build/bin/bb gates -b target/$prog.acir -c ../barretenberg/cpp/crs > target/$prog.circuit
 done
 
 echo "Processing completed."
